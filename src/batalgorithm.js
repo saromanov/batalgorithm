5c574b183b6129dd0286b242449dd144cd681559
Sergey Romanov <xxsmotur@gmail.com>
2015-10-12 23:00:30 +0500

    Update

--- src/batalgorithm.js
+++ src/batalgorithm.js
@@ -16,9 +16,14 @@ class BatAlgorithm {
     fit(f, X, iters, fmin, fmax, rates, A) {
         let iter = 0;
         let len = X.length;
+        let vprev = 0;
         while(iter < iters) {
             let rand = Math.Random();
             for(let i = 0;i < len;++i) {
+                //Generation of local solution
+                let f_i = fmin + (fmax - fmin)*beta;
+                let v = vprev + (x[i] - xbest) * f(x[i]);
+                let solution = x[i-1] + v;
                 if(rand > rates[i]) {
 
                 }
@@ -26,7 +31,9 @@ class BatAlgorithm {
                 if(rand < A[i] && f(X[i]) < f(Xbest)) {
 
                 }
+
             }
+            vprev = b;
         }
     }
 }
