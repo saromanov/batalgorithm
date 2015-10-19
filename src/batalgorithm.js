a9a507dce7ddf1b85ef5da4e7cf851f30e3b8876
Sergey Romanov <xxsmotur@gmail.com>
2015-10-19 19:43:36 +0500

    Update model

--- src/batalgorithm.js
+++ src/batalgorithm.js
@@ -13,20 +13,26 @@ class BatAlgorithm {
     //x - items
     //fmin, fmax - corresponds to a range of wavelengths
     //vector of rates
-    fit(f, X, iters, fmin, fmax, rates, A) {
+    fit(f, X, iters, rates, A, fmin=0, fmax=100) {
         let iter = 0;
         let len = X.length;
         let vprev = 0;
         let result = [];
+        //current population
+        let x = X;
+        let v = 0;
+        let xbest = x;
         while(iter < iters) {
             let rand = Math.Random();
             for(let i = 0;i < len;++i) {
                 //Generation of local solution
                 let f_i = fmin + (fmax - fmin)*beta;
                 let v = vprev + (x[i] - xbest) * f(x[i]);
-                let solution = x[i-1] + v;
+                x[i] = x[i-1] + v;
                 if(rand > rates[i]) {
-
+                    xbest = x;
+                    //Generate a local solution around the selected best solution
+                    xbest = xbest + eps * avg(A);
                 }
 
                 if(rand < A[i] && f(X[i]) < f(Xbest)) {
@@ -47,4 +53,18 @@ class BatAlgorithm {
             }
         }
     }
+}
+
+
+let avg = function(A) {
+    if(A.length == 0) {
+        return 0;
+    }
+    let result = 0;
+
+    for(let y in A) {
+        result += y;
+    }
+
+    return result/A.length;
 }
